d2fb3f338dbed815fb4c8fee386258439ead88dd
falkTX <falktx@falktx.com>
2026-08-07 11:33:24 +0200

    Bump release version
    Signed-off-by: falkTX <falktx@falktx.com>

--- src/AnagramControlPlugin.cpp
+++ src/AnagramControlPlugin.cpp
@@ -87,7 +87,7 @@ protected:
     */
     uint32_t getVersion() const noexcept override
     {
-        return d_version(1, 4, 0);
+        return d_version(1, 17, 0);
     }
 
     // ----------------------------------------------------------------------------------------------------------------
